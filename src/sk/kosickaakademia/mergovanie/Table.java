4cd6c17e2fc891cb9bca6a22775522cc69d07943
SimonSenic <simon.senic@kosickaakademia.sk>
2021-06-08 12:37:42 +0200

    Added new String

--- src/sk/kosickaakademia/mergovanie/Table.java
+++ src/sk/kosickaakademia/mergovanie/Table.java
@@ -2,8 +2,10 @@ package sk.kosickaakademia.mergovanie;
 
 public class Table {
     private String name;
+    private String material;
 
-    public Table(String name){
+    public Table(String name, String material){
         this.name = name;
+        this.material = material;
     }
 }
