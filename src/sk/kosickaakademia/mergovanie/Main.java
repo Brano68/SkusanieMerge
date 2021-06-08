86981d3d966d1558678fde8c94e691226b8b0390
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2021-06-08 12:49:07 +0200

    After merging and added another table

--- src/sk/kosickaakademia/mergovanie/Main.java
+++ src/sk/kosickaakademia/mergovanie/Main.java
@@ -3,5 +3,6 @@ package sk.kosickaakademia.mergovanie;
 public class Main {
     public static void main(String[] args) {
         Table table = new Table("Kitchen table", "Wood");
+        Table table1 = new Table("Living room table", "Stone");
     }
 }
